a41e1906f95b1e8fcaed13ae3391428bea680b8c
Eric Hardy <Eric.Hardy@univ-brest.fr>
2018-06-29 23:26:29 +0200

    player as member, before change to musicplayer

--- src/concertina/player/content/player.py
+++ src/concertina/player/content/player.py
@@ -1,20 +1,35 @@
 # -*- coding: utf-8 -*-
 # from plone.app.textfield import RichText
 # from plone.autoform import directives
-# from concertina.player import _
-from dexterity.membrane.content.member import IMember
+from concertina.player import _
+from dexterity.membrane.content.member import IEmail
+from dexterity.membrane.behavior.user import INameFromFullName
 from plone.dexterity.content import Container
 # from plone.namedfile import field as namedfile
 # from plone.supermodel import model
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
-# from zope import schema
+from zope import schema
 from zope.interface import implementer
 
 
-class IPlayer(IMember):
+class IPlayerNameFromFullName(INameFromFullName):
+    """
+    """
+
+
+class IPlayer(IEmail):
     """ Marker interface and Dexterity Python Schema for Player
     """
+    first_name = schema.TextLine(
+        title=_(u"First Name"),
+        required=True,
+    )
+
+    last_name = schema.TextLine(
+        title=_(u"Last Name"),
+        required=True,
+    )
 
     # directives.widget(level=RadioFieldWidget)
     # level = schema.Choice(
