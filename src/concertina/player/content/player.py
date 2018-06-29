7e54dad3749c46fc53c38ad166f483e95f4d50a2
Eric Hardy <Eric.Hardy@univ-brest.fr>
2018-06-29 19:39:41 +0200

    player content type

--- src/concertina/player/content/player.py
+++ src/concertina/player/content/player.py
@@ -8,10 +8,12 @@ from plone.supermodel.directives import fieldset
 from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
 from zope.interface import implementer
+
+from dexterity.membrane.content.member import IMember
 from concertina.player import _
 
 
-class IPlayer(model.Schema):
+class IPlayer(IMember):
     """ Marker interface and Dexterity Python Schema for Player
     """
 
